95291600e23e5493b705f84e406b4b4307c498cf
1Bronze <khdkkhdd@gmail.com>
2023-05-25 10:20:08 +0900

    [1장] 1. 티켓 판매 애플리케이션 구현하기

---
+++ src/main/java/com/khdkkhdd/chapter01/Audience.java
@@ -0,0 +1,13 @@
+package com.khdkkhdd.chapter01;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
---
+++ src/main/java/com/khdkkhdd/chapter01/Ticket.java
@@ -0,0 +1,9 @@
+package com.khdkkhdd.chapter01;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee () {
+        return fee;
+    }
+}
